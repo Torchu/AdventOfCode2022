19096528ed4eb6500d1464878151c79850fc1644
torchu <victor.cabritag@gmail.com>
2022-12-03 09:39:05 +0100

    day3 part2

--- Day 3/rucksackReorganization.py
+++ Day 3/rucksackReorganization.py
@@ -32,7 +32,17 @@ def calculate_priority(item: str) -> int:
         # Lower case letters priority is [1, 26]
         return ascii_value - 96
     else:
-        raise Exception("Invalid item")
+        raise Exception(f"{item} is an invalid item")
+    
+def get_shared_item_between_rucksacks(rucksack1: str, rucksack2: str, rucksack3: str) -> str:
+    """
+    Gets the only item that is shared between the three rucksacks.
+    :param rucksack1: The list of items in the first rucksack.
+    :param rucksack2: The list of items in the second rucksack.
+    :param rucksack3: The list of items in the third rucksack.
+    :return: The item that is shared between the three rucksacks.
+    """
+    return list(set(rucksack1) & set(rucksack2) & set(rucksack3))[0]
 
 print("Part I")
 # Reads the input
@@ -48,5 +58,29 @@ while(not eof):
         rucksack = get_rucksack(line)
         shared_item = get_shared_item(rucksack)
         total_priority += calculate_priority(shared_item)
+        
+print(total_priority)
+
+print("Part II")
+# Reads the input
+stream = open("Day 3/input.txt", "r")
+eof = False
+rucksack_trio = []
+total_priority = 0
+while(not eof):
+    line = stream.readline()
+    # Exit if we reach the end of the file
+    if line == "":
+        eof = True
+    else:
+        rucksack_trio.append(line.strip())
+        if len(rucksack_trio) == 3:
+            # Get the shared item between the three rucksacks
+            shared_item = get_shared_item_between_rucksacks(rucksack_trio[0], rucksack_trio[1], rucksack_trio[2])
+            # Calculate the priority of the shared item
+            priority = calculate_priority(shared_item)
+            total_priority += priority
+            # Reset the list of rucksacks
+            rucksack_trio = []
         
 print(total_priority)
